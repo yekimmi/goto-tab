9a0091fcd72e2557abe3842e057934bfa9fbe758
Mikey Fujihara <mikeyfujihara@gmail.com>
2011-08-29 22:48:20 -1000

    added -
    this key lets you go back to a recent visited tab. even works
    for clicking on tabs.

--- js/background.js
+++ js/background.js
@@ -1,6 +1,34 @@
+var cache = {
+  key : "tab_last_id",
+  update : function(id) {
+    localStorage.setItem(this.key, id);
+  },
+  get : function() {
+    var id = localStorage.getItem(this.key);
+    if (id != null) {
+      return parseInt(id);
+    }
+  },
+  has : function() {
+    return localStorage.getItem(this.key) != null;
+  }
+};
+var currentId;
+chrome.tabs.onSelectionChanged.addListener(function(tabId, selectInfo) {
+  if (currentId != null) {
+    cache.update(currentId);
+  }
+  currentId = tabId;
+});
 chrome.omnibox.onInputEntered.addListener(function(text) {
-  var id = parseInt(text);
-  if (!isNaN(id)) {
+  var id = null;
+  if (text == "-" || text == "_last_tab") {
+    // go to last if exists
+    id = cache.get();
+  } else {
+    id = parseInt(text);
+  }
+  if (id != null && !isNaN(id)) {
     chrome.tabs.update(id, {
       selected : true
     });
@@ -16,8 +44,15 @@ chrome.omnibox.onInputEntered.addListener(function(text) {
   }
 });
 chrome.omnibox.onInputChanged.addListener(function(search, suggest) {
+  var suggestions = [];
+  if (search == "-" && cache.has()) {
+    console.log("here");
+    suggestions.push({
+      content : "_last_tab",
+      description : "<match>goto last tab</match>"
+    });
+  }
   findTabs(search, function(tabs) {
-    var suggestions = [];
     for (tabIndex in tabs) {
       var tabInfo = tabs[tabIndex];
       var tab = tabInfo.tab;
