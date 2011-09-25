f31f20445ff3f0dbc15969cd59b018e5449950a8
Mikey Fujihara <mikeyfujihara@gmail.com>
2011-09-24 16:08:37 -1000

    refactored into classes

--- goto-tab/js/background.js
+++ goto-tab/js/background.js
@@ -1,60 +1,18 @@
-var cache = {
-  key : "tab_last_id",
-  update : function(id) {
-    localStorage.setItem(this.key, id);
-  },
-  get : function() {
-    var id = localStorage.getItem(this.key);
-    if (id != null) {
-      return parseInt(id);
-    }
-  },
-  clear : function() {
-    localStorage.removeItem(this.key);
-  },
-  has : function() {
-    return localStorage.getItem(this.key) != null;
-  }
-};
-// update current
-var currentId;
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-  // this is because we are closing the tab we are viewing
-  if (tabId == currentId) {
-    // make the current the history one
-    currentId = cache.get();
-  }
-  else if (tabId == cache.get()){
-    cache.clear();
-  }
-});
-chrome.tabs.onSelectionChanged.addListener(function(tabId, selectInfo) {
-  chrome.tabs.get(tabId, function(tab) {
-    var listPage = chrome.extension.getURL("page_select.html");
-    // dont store the listPage
-    if (listPage != tab.url) {
-      if (currentId != null) {
-        cache.update(currentId);
-      }
-      currentId = tabId;
-    }
-  });
-});
 chrome.omnibox.onInputEntered.addListener(function(text) {
   text = text.trim();
   // show page list
   // experimental
-//  if (text == "" || text == "_grid_view") {
-//    var listPage = chrome.extension.getURL("page_select.html");
-//    chrome.tabs.create({
-//      url : listPage
-//    });
-//    return;
-//  }
+  // if (text == "" || text == "_grid_view") {
+  // var listPage = chrome.extension.getURL("page_select.html");
+  // chrome.tabs.create({
+  // url : listPage
+  // });
+  // return;
+  // }
   var id = null;
-  if (text == "-" || text == "_last_tab") {
+  if (text == "-" || text == "-last") {
     // go to last if exists
-    id = cache.get();
+    id = MANAGER.history.getLast();
   } else {
     id = parseInt(text);
   }
@@ -76,19 +34,19 @@ chrome.omnibox.onInputEntered.addListener(function(text) {
 chrome.omnibox.onInputChanged.addListener(function(search, suggest) {
   var suggestions = [];
   search = search.trim();
-  if (search == "-" && cache.has()) {
+  if (search == "-" && MANAGER.history.hasLast()) {
     suggestions.push({
-      content : "_last_tab",
+      content : "-last",
       description : "last viewed tab"
     });
   }
   // this was just experimental grid stuff
-//  if (search == "") {
-//    suggestions.push({
-//      content : "_grid_view",
-//      description : "show grid view"
-//    });
-//  }
+  // if (search == "") {
+  // suggestions.push({
+  // content : "_grid_view",
+  // description : "show grid view"
+  // });
+  // }
   findTabs(search, function(tabs) {
     for (tabIndex in tabs) {
       var tabInfo = tabs[tabIndex];
@@ -96,10 +54,12 @@ chrome.omnibox.onInputChanged.addListener(function(search, suggest) {
       var findIndex = tabInfo.index;
       suggestions.push({
         content : tab.id + "",
-        description : "<dim>" + encodeSpecial(tab.title.substring(0, findIndex))
+        description : "<dim>"
+            + encodeSpecial(tab.title.substring(0, findIndex))
             + "</dim><match>"
-            + encodeSpecial(tab.title.substring(findIndex, findIndex + search.length))
-            + "</match><dim>" + encodeSpecial(tab.title.substring(findIndex + search.length))
+            + encodeSpecial(tab.title.substring(findIndex, findIndex
+                + search.length)) + "</match><dim>"
+            + encodeSpecial(tab.title.substring(findIndex + search.length))
             + "</dim>"
       });
     }
--- goto-tab/js/commons.js
+++ goto-tab/js/commons.js
@@ -1,8 +1,50 @@
-function encodeSpecial(toEncode){
+function TabHistory() {
+  this.removeCallback = function(tabId, removeInfo) {
+    // this is because we are closing the tab we are viewing
+    if (tabId == this.currentId) {
+      // make the current the history one
+      this.currentId = this.lastTabId;
+    } else if (tabId == this.lastTabId) {
+      delete this.lastTabId;
+    }
+  };
+  this.changeCallback = function(tabId, selectInfo) {
+    var self = this;
+    chrome.tabs.get(tabId, function(tab) {
+      var listPage = chrome.extension.getURL("page_select.html");
+      // dont store the listPage
+      if (listPage != tab.url) {
+        if (self.currentId != null) {
+          self.lastTabId = self.currentId;
+        }
+        self.currentId = tabId;
+      }
+    });
+  };
+  this.hasLast = function() {
+    return this.lastTabId != undefined;
+  };
+  this.getLast = function() {
+    return this.lastTabId;
+  };
+};
+function TabManager() {
+  this.history = new TabHistory();
+};
+
+var MANAGER = new TabManager();
+// initialize the callbacks
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  MANAGER.history.removeCallback(tabId, removeInfo);
+});
+chrome.tabs.onSelectionChanged.addListener(function(tabId, selectInfo) {
+  MANAGER.history.changeCallback(tabId, selectInfo);
+});
+function encodeSpecial(toEncode) {
   var text = toEncode;
-  text = text.replace(/&/g,"&amp;");
-  text = text.replace(/</g,"&lt;");
-  text = text.replace(/>/g,"&gt;");
+  text = text.replace(/&/g, "&amp;");
+  text = text.replace(/</g, "&lt;");
+  text = text.replace(/>/g, "&gt;");
   return text;
 }
 function findTabs(search, callback) {
